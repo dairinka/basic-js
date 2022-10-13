0313b49a327456250a918a24e549acf785cf7264
dairinka <104695300+dairinka@users.noreply.github.com>
2022-10-14 02:25:49 +0300

    feat: add solution

--- src/dream-team.js
+++ src/dream-team.js
@@ -13,9 +13,22 @@ const { NotImplementedError } = require('../extensions/index.js');
  * createDreamTeam(['Olivia', 1111, 'Lily', 'Oscar', true, null]) => 'LOO'
  *
  */
-function createDreamTeam(/* members */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function createDreamTeam(members) {
+  if (members == null || typeof members[Symbol.iterator] !== "function") {
+    return false;
+  }
+  let abrArray = [];
+  for (const member of members) {
+   
+    if (typeof member == "string") {
+      let correctMember = member.trim();
+      abrArray.push(correctMember[0].toUpperCase());
+    }
+   
+  }
+  let result = abrArray.sort().join("");
+  
+  return result || false;
 }
 
 module.exports = {
--- src/what-season.js
+++ src/what-season.js
@@ -11,9 +11,42 @@ const { NotImplementedError } = require('../extensions/index.js');
  * getSeason(new Date(2020, 02, 31)) => 'spring'
  * 
  */
-function getSeason(/* date */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function getSeason(date) {
+  let dateWrapper = new Date(date);
+  if (date == null || date == "underfined") {
+    return "Unable to determine the time of year!";
+  }
+  if (isNaN(dateWrapper.getDate())) {
+    throw new Error("Invalid date!");
+  }
+  if (
+    typeof date.toString !== "function" ||
+    typeof date.getFullYear !== "function" ||
+    typeof date.getDate !== "function" ||
+    typeof date.getHours !== "function" ||
+    typeof date.getMinutes !== "function" ||
+    typeof date.getSeconds !== "function" ||
+    typeof date.getMilliseconds !== "function" ||
+    typeof date.getDay !== "function" ||
+    !(date instanceof Date)
+  ) {
+    throw new Error("Invalid date!");
+  }
+  
+  
+    let month = date.getMonth();
+ 
+
+  if (month < 2 || month == 11) {
+    return "winter";
+  } else if (month < 5) {
+    return "spring";
+  } else if (month < 8) {
+    return "summer";
+  } else if (month < 11) {
+    return "fall";
+  }
+  throw new Error("Invalid date!");
 }
 
 module.exports = {
